df70ca895a8b6e2af17933d4d71fb16b1f2e9d12
aimeedeer <aimeedeer@gmail.com>
2020-10-26 16:03:06 -0500

    update emacs

--- init.el
+++ init.el
@@ -4,23 +4,37 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
-
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 
+(setq byte-compile-warnings '(cl-functions))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(muse-project-alist nil)
- '(package-selected-packages '(org-brain muse ## js2-mode rust-mode)))
+ '(package-selected-packages '(org-roam ## js2-mode rust-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(setq org-roam-directory "~/github/org-roam")
+
+(add-hook 'after-init-hook 'org-roam-mode)
+(setq org-roam-tag-sources '(prop last-directory))
+
+(require 'org-roam-protocol)
+
+(make-org-roam-doctor-checker
+ :name 'org-roam-doctor-broken-links
+ :description "Fix broken links."
+ :actions '(("d" . ("Unlink" . org-roam-doctor--remove-link))
+	    ("r" . ("Replace link" . org-roam-doctor--replace-link))
+	                   ("R" . ("Replace link (keep label)" . org-roam-doctor--replace-link-keep-label))))
+(setq org-roam-db-gc-threshold most-positive-fixnum)
 
 ;; -*- mode: elisp -*-
 
@@ -49,4 +63,4 @@
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 (el-get 'sync)
 
-(add-to-list 'load-path "~/.emacs.d/org/roam")
+
